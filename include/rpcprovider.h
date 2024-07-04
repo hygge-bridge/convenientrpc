07e387e7eaf40d33a67201321190646e3ad157c3
hygge <2078897125@qq.com>
2024-07-04 21:48:21 +0800

    Add 实现rpcprovider的发布服务模块

--- include/rpcprovider.h
+++ include/rpcprovider.h
@@ -3,11 +3,14 @@
 
 #include "rpcapplication.h"
 #include "google/protobuf/service.h"
+#include "google/protobuf/descriptor.h"
 #include "muduo/net/TcpServer.h"
 #include "muduo/net/EventLoop.h"
 #include "muduo/net/InetAddress.h"
 #include <string>
+#include <unordered_map>
 
+// rpc服务提供类
 class RpcProvider {
 public:
     // 发布rpc服务
@@ -23,6 +26,17 @@ private:
     // 处理可读写事件的回调函数
     void OnMessage(const muduo::net::TcpConnectionPtr& conn, 
                     muduo::net::Buffer* buffer, muduo::Timestamp time);
+
+    // 服务的详细信息
+    struct ServiceInfo {
+        // service指针，用于后续底层调用上层的服务
+        google::protobuf::Service* service;  
+        // 方法名与方法描述的映射表
+        std::unordered_map<std::string, const google::protobuf::MethodDescriptor*> method_map;  
+    };
+    // 服务名与服务信息的映射表
+    std::unordered_map<std::string, ServiceInfo> service_map_;
+
     muduo::net::EventLoop event_loop_;
 };
 
--- src/rpcprovider.cc
+++ src/rpcprovider.cc
@@ -4,8 +4,24 @@
 // 根据cpu核心数量来设置线程个数
 static const int kThreadNum = std::thread::hardware_concurrency();
 
+// 发布服务需要将对应服务和其方法的 名字和描述信息转入映射表中，
+// 这样后续rpc框架才可以根据该映射表找到对应的方法
 void RpcProvider::NotifyService(google::protobuf::Service* service) {
-
+    // 获取服务名字和描述
+    const google::protobuf::ServiceDescriptor* service_descriptor = service->GetDescriptor();
+    const std::string service_name = service_descriptor->name();
+    int method_cnt = service_descriptor->method_count();
+    ServiceInfo service_info;
+    std::cout << "service name: " << service_name << std::endl;
+    // 获取方法名字和描述
+    for (int i = 0 ; i < method_cnt; ++i) {
+        const google::protobuf::MethodDescriptor *method_descriptor = service_descriptor->method(i);
+        const std::string method_name = method_descriptor->name();
+        service_info.method_map.insert({method_name, method_descriptor});
+        std::cout << "method name: " << method_name << std::endl;
+    }
+    service_info.service = service;
+    service_map_.insert({service_name, service_info});
 }
 
 void RpcProvider::Run() {
@@ -14,9 +30,7 @@ void RpcProvider::Run() {
     muduo::net::InetAddress address(ip, port);
     muduo::net::TcpServer tcp_server(&event_loop_, address, "rpcprovider");
     tcp_server.setThreadNum(kThreadNum);  
-
     std::cout << "rpcprovider start: ip=" << ip << " port=" << port << std::endl;
-
     tcp_server.start();
     event_loop_.loop();
 }
