f5e7083ccee2354ab7b413cfef382f103446cbdd
hygge <2078897125@qq.com>
2024-07-11 11:36:19 +0800

    Fix 修复因为变量名冲突导致的coredump

--- include/messagequeue.h
+++ include/messagequeue.h
@@ -11,22 +11,22 @@ class MessageQueue {
 public:
     void Push(const T& data) {
         std::lock_guard<std::mutex> lock(mutex_);
-        msg_queue_.push(data);
+        queue_.push(data);
         cond_variable_.notify_one();
     }
 
     T Pop() {
         std::unique_lock<std::mutex> lock(mutex_);
-        while (msg_queue_.empty()) {
+        while (queue_.empty()) {
             cond_variable_.wait(lock);
         }
-        T data = msg_queue_.front();
-        msg_queue_.pop();
+        T data = queue_.front();
+        queue_.pop();
         return data;
     }
 
 private:
-    std::queue<T> msg_queue_;
+    std::queue<T> queue_;
     std::mutex mutex_;
     std::condition_variable cond_variable_;
 };
